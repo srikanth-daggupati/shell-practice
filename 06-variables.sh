9deb38e200c9539a89ce49c9552d93edd751449c
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-01-28 11:58:39 -0600

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -2,11 +2,11 @@
 
 START_TIME=$(date +%s)
 
-echo "Script excuted at:$TIMESTAMP"
+echo "Script excuted at: $START_TIME"
 
 sleep 10
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 
-echo"Script excuted in:$TOTAL_TIME"
+echo"Script excuted in: $TOTAL_TIME seconds"
