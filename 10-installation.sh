e359ede38f4fc23a53a42cf1b5977d25b2cc30ad
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-01-29 09:10:33 -0600

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+echo"Installing Nginx"
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx ... Failed"
+    exit 1
+else
+    echo "Installing Nginx ... Success"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing MySQL ... Failed"
+    exit 1
+else
+    echo "Installing MySQL ... Success"
+fi
+
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing NodeJS ... Failed"
+    exit 1
+else
+    echo "Installing NodeJS ... Success"
+fi
