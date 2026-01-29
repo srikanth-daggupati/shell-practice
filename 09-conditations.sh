528555058db7497bd874236959b205dd1dda9e2a
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-01-28 19:03:15 -0600

    actions

--- 09-conditations.sh
+++ 09-conditations.sh
@@ -6,8 +6,10 @@ NUMBER=$1
 # -lt -> Less Than
 # -eq -> equal
 # -ne -> not equal
-if [ $NUMBER -gt25 ]; then
+if [ $NUMBER -gt 25 ]; then
     echo "Given number: $NUMBER is greater than 25"
+elif [$NUMBER -eq 25]; then
+    echo "Given number: $NUMBER is equal to 25"
 else
-    echo "Given number: $NUmber is not greater than25"
+    echo "Given number: $NUMBER is not greater than 25"
 fi
