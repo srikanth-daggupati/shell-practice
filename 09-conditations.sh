15e7c7d58afa966a1ace913a29e1ed380dfa3f60
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-01-28 18:37:46 -0600

    actions

---
+++ 09-conditations.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=25
+
+# -gt -> Greater Than
+# -lt -> Less Than
+# -eq -> equal
+# -ne -> not equal
+if [ $NUMBER -gt 20]; then
+    echo "Given number : $NUMBER is greater than 20"
+fi
