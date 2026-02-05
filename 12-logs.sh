6ada4c005698239edcba4f4eb7b0c6b5a778b032
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-02-05 14:11:38 -0600

    actions

--- 12-logs.sh
+++ 12-logs.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access"
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -12,10 +12,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $? -ne 0 ]; then
-        echo "$2 ...Failed"
+        echo "$2 ...Failed" | tee -a $LOGS_FILE
         exit 1
     else 
-        echo "$2 ...Success"
+        echo "$2 ...Success" | tee -a $LOGS_FILE
     fi
 }
 
